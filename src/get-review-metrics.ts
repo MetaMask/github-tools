9b12d812276146bb1c48a4fc90aded95c8d83f99
Elliot Winkler <elliot.winkler@gmail.com>
2023-08-28 17:43:55 -0600

    Sync with module template
    - Bump ESLint packages and apply formatting fixes
    - Synchronize Dependabot configuration file

--- src/get-review-metrics.ts
+++ src/get-review-metrics.ts
@@ -1,4 +1,5 @@
-import { Octokit, RestEndpointMethodTypes } from '@octokit/rest';
+import type { RestEndpointMethodTypes } from '@octokit/rest';
+import { Octokit } from '@octokit/rest';
 import { DateTime } from 'luxon';
 
 const octokit = new Octokit();
