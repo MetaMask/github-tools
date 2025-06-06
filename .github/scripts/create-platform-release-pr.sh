f5f37f688d90dde1a40446ec07299334fccd3370
Alejandro Som <560018+alucardzom@users.noreply.github.com>
2025-06-06 13:44:24 +0200

    chore: add conditional for updating the spreadsheet
    For testing, we don't wanna execute the spreadsheet step, just create
    the commits csv file.

--- .github/scripts/create-platform-release-pr.sh
+++ .github/scripts/create-platform-release-pr.sh
@@ -184,9 +184,11 @@ yarn --cwd install
 echo "Generating test plan csv.."
 yarn run gen:commits "${PLATFORM}" "${PREVIOUS_VERSION}" "${RELEASE_BRANCH_NAME}" "${PROJECT_GIT_DIR}"
 
-echo "Updating release sheet.."
-# Create a new Release Sheet Page for the new version with our commits.csv content
-yarn run update-release-sheet "${PLATFORM}" "${NEW_VERSION}" "${GOOGLE_DOCUMENT_ID}" "./commits.csv" "${PROJECT_GIT_DIR}" "${MOBILE_TEMPLATE_SHEET_ID}" "${EXTENSION_TEMPLATE_SHEET_ID}"
+if [[ "${TEST_ONLY:-true}" != "true" ]]; then
+  echo "Updating release sheet.."
+  # Create a new Release Sheet Page for the new version with our commits.csv content
+  yarn run update-release-sheet "${PLATFORM}" "${NEW_VERSION}" "${GOOGLE_DOCUMENT_ID}" "./commits.csv" "${PROJECT_GIT_DIR}" "${MOBILE_TEMPLATE_SHEET_ID}" "${EXTENSION_TEMPLATE_SHEET_ID}"
+fi
 cd ../
 
 # Commit and Push Changelog Changes
