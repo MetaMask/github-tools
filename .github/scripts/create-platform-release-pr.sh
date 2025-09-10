f3d68973e08f6319fd8503cf63b02c0cfd579257
Pavel Dvorkin <pavel.dvorkin@consensys.net>
2025-09-10 10:17:23 -0400

    Test changelog automation https://github.com/MetaMask/auto-changelog/pull/247

--- .github/scripts/create-platform-release-pr.sh
+++ .github/scripts/create-platform-release-pr.sh
@@ -319,7 +319,7 @@ create_changelog_pr() {
 
     # Generate Changelog and Test Plan
     echo "Generating changelog via auto-changelog.."
-    npx @metamask/auto-changelog@4.1.0 update --rc --repo "${GITHUB_REPOSITORY_URL}" --currentVersion "${new_version}" --autoCategorize  --useChangelogEntry --useShortPrLink
+    npx github:MetaMask/auto-changelog#parse-changelog-entry-adjustments update --rc --repo "${GITHUB_REPOSITORY_URL}" --currentVersion "${new_version}" --autoCategorize  --useChangelogEntry --useShortPrLink
 
     # Skip commits.csv for hotfix releases (previous_version_ref is literal "null")
     # - When we create a new major/minor release, we fetch all commits included in the release, by fetching the diff between HEAD and previous version reference.
